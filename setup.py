22c051cd04f36734a9d39ef5480f6cf50e14e25b
Khizer-khan701 <khizerdawood987@gmail.com>
2025-09-18 10:55:10 +0500

    requirements done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages # type: ignore
+
+setup(
+    name="AI-Powered-Resume-Job-Match-System",
+    author="Khizer Dawood",
+    author_email="khizerkhan81009@gmail.com",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[]
+)
